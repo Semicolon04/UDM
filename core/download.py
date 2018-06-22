546f28357c283612389ee99783d8662568a669d9
Sivakar Sithamparanathan <sivakar12@outlook.com>
2018-06-22 15:18:11 +0530

    Implement get_size()

--- core/download.py
+++ core/download.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 from os.path import join, basename
+from urllib.request import urlopen, Request
 
-
-homepath = Path.home()
+default_download_path = join(Path.home(), 'Downloads')
 
 STATE_PAUSED = 0
 STATE_DOWNLOADING = 1
@@ -11,18 +11,25 @@ STATE_ERROR = 3
 
 class Download:
     def __init__(self, url, directory=None, filename=None):
-        self.url = url
-        self.directory = directory if directory else homepath
+        self.source_url = url
+        self.directory = directory if directory else default_download_path
         self.filename = filename if filename else basename(url)
-        self.total_byes = self._get_size()
+        self.total_bytes = self._get_size()
         self.bytes_downloaded = 0
         self.state = STATE_PAUSED
 
+    def __str__(self):
+        return 'Download from %s to %s' % (self.source_url, self.get_destination_path())
+
     def _get_size(self):
-        pass
+        request_to_send = Request(self.source_url, method='HEAD')
+        return response.getheader('Content-Length')
     
     def _create_file(self):
         pass
+    
+    def get_destination_path(self):
+        return join(self.directory, self.filename)
     
     def start_downloading(self):
         pass
--- download_test.py
+++ download_test.py
@@ -3,3 +3,6 @@ from core.download import Download
 url = 'https://upload.wikimedia.org/wikipedia/en/thumb/d/d8/Game_of_Thrones_title_card.jpg/250px-Game_of_Thrones_title_card.jpg'
 download = Download(url)
 print(download)
+print(download.source_url)
+print(download.get_destination_path())
+print(download.total_bytes)
